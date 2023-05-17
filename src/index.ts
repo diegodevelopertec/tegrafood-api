2b6bc821a55c8049c2600ccfcb2bf2ac646d7d7a
Diego Morais <diegodutramorais@gmail.com>
2023-05-17 03:37:55 -0300

    deploy na vercel

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import Routes from './Routes'
 import path from 'path'
 import cors from 'cors'
-
+import { Request,Response } from 'express'
 
 
 dotenv.config()
@@ -15,4 +15,15 @@ api.use(cors({origin:'*'}))
 api.use(express.urlencoded({extended:true}))
 api.use(express.static(path.join(__dirname,'../public/')))
 api.use(Routes)
+api.use('',(req:Request,res:Response)=>{
+    res.send(`Bem Vindo á api TegraFood.Aqui você poderá acessar os seguintes eadpoints:
+    <ul>
+        <li>GET <i>products</i></li>
+        <li>GET <i>products/:id</i></li>
+        <li>POST  <i>products</i></li>
+        <li>UPDATE  <i>products/:id</i></li>
+        <li>DELETE  <i>products/:id</i></li>
+    </ul>
+    `)
+})
 api.listen(process.env.PORT,()=>console.log(`servidor rodando em http://localhost:${process.env.PORT}`) )
